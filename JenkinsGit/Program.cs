ded30c1509fa219b2601f328b8f0e526adcd0718
Vasanth <vasanthgabriel88@gmail.com>
2021-09-09 09:23:09 +0400

    Add project files.

---
+++ JenkinsGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jenkins!");
+        }
+    }
+}
